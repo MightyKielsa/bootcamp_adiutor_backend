718f462dbc8d39168baad069345124ed93cec752
MightyKielsa <104023970+MightyKielsa@users.noreply.github.com>
2022-06-21 14:41:37 +0100

    help table created with 6 help connections, resource table created with 4 res

---
+++ libs/help.js
@@ -0,0 +1,30 @@
+//help (helpID INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, userID INT, topicID INT);
+
+//DUMMY DATA ONLY FOR DEMONSTRATION AND TESTING PURPOSES
+
+export const help = [
+  {
+    userID: 3,
+    topicID: 2,
+  },
+  {
+    userID: 2,
+    topicID: 1,
+  },
+  {
+    userID: 3,
+    topicID: 1,
+  },
+  {
+    userID: 4,
+    topicID: 4,
+  },
+  {
+    userID: 6,
+    topicID: 1,
+  },
+  {
+    userID: 5,
+    topicID: 3,
+  },
+];
---
+++ libs/resource.js
@@ -0,0 +1,32 @@
+//resource (resourceID INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, userID INT, topicID INT, tags TEXT [], link TEXT, rating INT)
+
+export const resource = [
+  {
+    userID: 1,
+    topicID: 1,
+    tags: ["html", "video"],
+    link: "https://www.youtube.com/watch?v=G1IbRujko-A",
+    rating: 5,
+  },
+  {
+    userID: 3,
+    topicID: 1,
+    tags: ["html", "image"],
+    link: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.emagidla.com%2Fwp-content%2Fuploads%2F2013%2F10%2Fhtml5.png&f=1&nofb=1",
+    rating: 3,
+  },
+  {
+    userID: 4,
+    topicID: 4,
+    tags: ["javascript", "video"],
+    link: "https://www.youtube.com/watch?v=BggrpKfqh1c",
+    rating: 1,
+  },
+  {
+    userID: 2,
+    topicID: 3,
+    tags: ["css", "article"],
+    link: "https://www.w3schools.com/quiztest/quiztest.asp?qtest=CSS",
+    rating: 4,
+  },
+];
