d1da8012250bfff3049f7af896f169c5e1ce4c15
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-06-22 16:40:04 +0100

    more dummy data added to happy to help

--- libs/help.js
+++ libs/help.js
@@ -24,7 +24,55 @@ export const help = [
     topicID: 1,
   },
   {
-    userID: 5,
+    userID: 10,
+    topicID: 1,
+  },
+  {
+    userID: 18,
+    topicID: 4,
+  },
+  {
+    userID: 17,
     topicID: 3,
+  },
+  {
+    userID: 15,
+    topicID: 4,
+  },
+  {
+    userID: 1,
+    topicID: 1,
+  },
+  {
+    userID: 2,
+    topicID: 3,
+  },
+  {
+    userID: 1,
+    topicID: 4,
+  },
+  {
+    userID: 19,
+    topicID: 4,
+  },
+  {
+    userID: 13,
+    topicID: 3,
+  },
+  {
+    userID: 12,
+    topicID: 1,
+  },
+  {
+    userID: 8,
+    topicID: 4,
+  },
+  {
+    userID: 7,
+    topicID: 2,
+  },
+  {
+    userID: 5,
+    topicID: 1,
   },
 ];
